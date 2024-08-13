5db37caf642020656a36587c80c41b0a92f5b16d
vito di venosa <110898637+Vito290500@users.noreply.github.com>
2024-08-13 15:50:55 +0200

    n-1 improvment update

---
+++ app.js
@@ -0,0 +1,52 @@
+const followBtn = document.getElementById('follow-btn')
+const newsModal = document.getElementById('news-modal')
+const closeNewsBtn = document.getElementById('news-modal')
+
+const backdrop = document.getElementById('backdrop')
+
+const shareBtn = document.getElementById('share-btn')
+const shareModal = document.getElementById('share-modal')
+const closeShareBtn = document.getElementById('closeShareBtn')
+
+function openNewsModal() {
+    newsModal.classList.add('show');
+    backdrop.style.display = 'block';
+    setTimeout(() => {
+        newsModal.style.opacity = '1';
+    }, 10);
+}
+
+function openShareModal() {
+    shareModal.classList.add('show');
+    backdrop.style.display = 'block';
+    setTimeout(() => {
+        shareModal.style.opacity = '1';
+    }, 10);
+}
+
+function closeNewsModal() {
+    newsModal.style.opacity = '0';
+    backdrop.style.display = 'none';
+    setTimeout(() => {
+        newsModal.classList.remove('show');
+    }, 300);
+}
+
+function closeShareModal() {
+    shareModal.style.opacity = '0';
+    backdrop.style.display = 'none';
+    setTimeout(() => {
+       shareModal.classList.remove('show');
+    }, 300);
+}
+
+function closeall(){
+    closeNewsModal()
+    closeShareModal()
+}
+
+shareBtn.addEventListener('click', openShareModal)
+closeShareBtn.addEventListener('click', closeShareModal)
+backdrop.addEventListener('click', closeall)
+closeNewsBtn.addEventListener('click', closeNewsModal)
+followBtn.addEventListener('click', openNewsModal)
